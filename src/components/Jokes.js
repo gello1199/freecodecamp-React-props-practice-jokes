4cb7ef6a3c0a027068194143a0bc350a37569c80
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-27 11:44:56 -0500

    Added conditional to punchline to toggle

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -10,7 +10,7 @@ const Jokes = (props) => {
     return (
         <div>
             {props.setup && <h3>Setup: {props.setup}</h3>}
-            <p>Punchline: {props.punchline}</p>
+            {isShown && <p>Punchline: {props.punchline}</p>}
             <button onClick={toggleShown}>Show Punchline</button>
             <hr />
         </div>
